9c0a594e4300badf23827676eadf1b7140c773b6
Felix Jentzsch <felix.jentzsch@upb.de>
2025-01-20 10:03:46 +0000

    Extend CI to full test suite, workaround for onnxruntime on slurm

--- docker/quicktest.sh
+++ docker/quicktest.sh
@@ -15,7 +15,8 @@ elif [ $1 = "rtlsim" ]; then
   pytest -k rtlsim --workers $PYTEST_PARALLEL
 elif [ $1 = "end2end" ]; then
   echo "Running end2end test suite with no parallelism"
-  pytest -k end2end
+# filtering by name "end2end" is not sufficient, as the bnn tests need to be selected by a marker
+  pytest -k end2end -m 'sanity_bnn or end2end or fpgadataflow or notebooks'
 elif [ $1 = "full" ]; then
   echo "Running full test suite, each step with appropriate parallelism"
   $0 main;
