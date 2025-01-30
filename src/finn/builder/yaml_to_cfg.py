76a8927d89e19e9a3240bfe7c021afafa77221fc
bwintermann <bjarne.wintermann@me.com>
2025-01-30 15:33:51 +0100

    Completed implementing all possible arguments to BuildDataflowConfig

--- src/finn/builder/yaml_to_cfg.py
+++ src/finn/builder/yaml_to_cfg.py
@@ -6,8 +6,16 @@ from importlib import import_module
 from pathlib import Path
 from typing import Any
 
-from finn.builder import build_dataflow_steps
-from finn.builder.build_dataflow_config import DataflowBuildConfig, DataflowOutputType
+from finn.builder import build_dataflow, build_dataflow_steps
+from finn.builder.build_dataflow_config import (
+    AutoFIFOSizingMethod,
+    DataflowBuildConfig,
+    DataflowOutputType,
+    FpgaMemoryType,
+    LargeFIFOMemStyle,
+    ShellFlowType,
+    VitisOptStrategy,
+)
 
 
 def variant_from_str(enum_class, variant) -> Any:
@@ -20,8 +28,52 @@ def variant_from_str(enum_class, variant) -> Any:
 def try_insert_setting(
     setting: str, general_config: dict, cfg: DataflowBuildConfig
 ) -> DataflowBuildConfig:
-    if setting in general_config.keys() and setting in cfg.__dict__.keys():
-        cfg.__setattr__(setting, general_config[setting])
+    if setting in general_config.keys():
+        if setting in cfg.__dict__.keys():
+            cfg.__setattr__(setting, general_config[setting])
+        else:
+            print(f"Unknown setting key {setting}. Skipping")
+    return cfg
+
+
+def try_insert_setting_enum(
+    enum_setting: tuple, general_config: dict, cfg: DataflowBuildConfig
+) -> DataflowBuildConfig:
+    setting_name, enum_type = enum_setting
+    if enum_setting in general_config.keys():
+        if enum_setting in cfg.__dict__.keys():
+            cfg.__setattr__(enum_setting, variant_from_str(enum_type, general_config[enum_setting]))
+        else:
+            print(f"Unknown enum setting key {enum_setting}. Skipping")
+    return cfg
+
+
+def process_steps(
+    p: Path, data: dict, cfg: DataflowBuildConfig, step_type: str
+) -> DataflowBuildConfig | None:
+    if step_type in data.keys():
+        steps = data[step_type]
+        used_steps = []
+        for step in steps:
+            if step in build_dataflow_steps.build_dataflow_step_lookup.keys():
+                used_steps.append(step)
+            else:
+                # The module name is everything except the last qualifier
+                mod_name = ".".join(step.split(".")[:-1])
+                step_name = step.split(".")[-1]
+
+                # Add the custom step module path to PATH so python can import it
+                # Assumes that build.py and custom_steps.py are in the same dir
+                sys.path.append(str(p.parent.absolute()))
+
+                # Import the step and add it to the list
+                custom_step_module = import_module(mod_name)
+                if step_name in dir(custom_step_module):
+                    used_steps.append(getattr(custom_step_module, step_name))
+                else:
+                    print(f"Step {step} is neither an integrated step nor importable via module")
+                    return None
+        cfg.steps = used_steps
     return cfg
 
 
@@ -59,46 +111,57 @@ def buildcfg_from_yaml(p: Path) -> DataflowBuildConfig | None:
             "target_fps",
             "folding_two_pass_relaxation",
             "board",
-            "shell_flow_type",
             "vitis_platform",
-            "vitis_opt_strategy",
             "specialize_layers_config_file",
             "folding_config_file",
             "auto_fifo_depths",
             "split_large_fifos",
             "rtlsim_batch_size",
             "save_intermediate_models",
+            "verify_input_npy",
+            "verify_expected_output_npy",
+            "verify_save_full_context",
+            "verify_save_rtlsim_waveforms",
+            "stitched_ip_gen_dcp",
+            "signature",
+            "minimize_bit_widths",
+            "fpga_part",
+            "force_python_rtlsim",
+            "large_fifo_mem_style",
+            "hls_clk_period_ns",
+            "default_swg_exception",
+            "vitis_floorplan_file",
+            "enable_hw_debug",
+            "enable_build_pdb_debug",
+            "verbose",
+            "start_step",
+            "stop_step",
+            "max_multithreshold_bit_width",
+            "rtlsim_use_vivado_comps",
         ]
         for setting in general_settings:
             cfg = try_insert_setting(setting, general, cfg)
 
+        # Setting the enums
+        general_enum_settings = [
+            ("shell_flow_type", ShellFlowType),
+            ("auto_fifo_strategy", AutoFIFOSizingMethod),
+            ("large_fifo_mem_style", LargeFIFOMemStyle),
+            ("vitis_opt_strategy", VitisOptStrategy),
+            ("fpga_memory", FpgaMemoryType),
+        ]
+        for enum_setting in general_enum_settings:
+            cfg = try_insert_setting_enum(enum_setting, general, cfg)
+
         # Build steps list
         # Existing steps are simply passed
         # If the format is module_name.step_name, module_name is automatically imported
-        if "build_steps" in data.keys():
-            steps = data["build_steps"]
-            used_steps = []
-            for step in steps:
-                if step in build_dataflow_steps.build_dataflow_step_lookup.keys():
-                    used_steps.append(step)
-                else:
-                    # The module name is everything except the last qualifier
-                    mod_name = ".".join(step.split(".")[:-1])
-                    step_name = step.split(".")[-1]
-
-                    # Add the custom step module path to PATH so python can import it
-                    # Assumes that build.py and custom_steps.py are in the same dir
-                    sys.path.append(str(p.parent.absolute()))
-
-                    # Import the step and add it to the list
-                    custom_step_module = import_module(mod_name)
-                    if step_name in dir(custom_step_module):
-                        used_steps.append(getattr(custom_step_module, step_name))
-                    else:
-                        print(
-                            f"Step {step} is neither an integrated step nor importable via module"
-                        )
-                        return None
-            cfg.steps = used_steps
+        cfg = process_steps(p, data, cfg, "steps")
+        cfg = process_steps(p, data, cfg, "verify_steps")
         return cfg
     return None
+
+
+def run_finn_from_yaml(buildfile: str, modelfile: str) -> None:
+    """Entrypoint for the FINN flow when using a YAML build file"""
+    build_dataflow.build_dataflow_cfg(modelfile, buildcfg_from_yaml(Path(buildfile)))
