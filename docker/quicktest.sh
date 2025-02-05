2ad62a3ef6f3a1c9a3d93adc9f1cbcff59564856
Felix Jentzsch <felix.jentzsch@upb.de>
2025-02-05 10:49:14 +0000

    Try worksteal test distribution

--- docker/quicktest.sh
+++ docker/quicktest.sh
@@ -6,18 +6,18 @@ cd $FINN_ROOT
 # check if command line argument is empty or not present
 if [ -z $1 ] || [ $1 = "quicktest" ]; then
   echo "Running quicktest: not (vivado or slow or board) with pytest-xdist"
-  pytest -m 'not (vivado or slow or vitis or board or notebooks or bnn_pynq)' --forked --dist loadfile -n $PYTEST_PARALLEL
+  pytest -m 'not (vivado or slow or vitis or board or notebooks or bnn_pynq)' --junitxml=reports/quick.xml --html=reports/quick.html --reruns 2 --forked --dist worksteal -n $PYTEST_PARALLEL
 elif [ $1 = "main" ]; then
   echo "Running main test suite: not (rtlsim or end2end) with pytest-xdist"
-  pytest -k 'not (rtlsim or end2end)' --junitxml=main.xml --html=reports/main.html --reruns 1 --forked --dist loadfile -n $PYTEST_PARALLEL
+  pytest -k 'not (rtlsim or end2end)' --junitxml=reports/main.xml --html=reports/main.html --reruns 2 --forked --dist worksteal -n $PYTEST_PARALLEL
 elif [ $1 = "rtlsim" ]; then
   echo "Running rtlsim test suite with pytest-xdist"
 # there are end2end tests which also have rtlsim in their name, but not vice versa
-  pytest -k 'rtlsim and not end2end' --junitxml=rtlsim.xml --html=reports/rtlsim.html --reruns 1 --forked --dist loadfile -n $PYTEST_PARALLEL
+  pytest -k 'rtlsim and not end2end' --junitxml=reports/rtlsim.xml --html=reports/rtlsim.html --reruns 2 --forked --dist worksteal -n $PYTEST_PARALLEL
 elif [ $1 = "end2end" ]; then
   echo "Running end2end test suite with no parallelism"
 # filtering by name "end2end" is not sufficient, as the bnn tests need to be selected by a marker
-  pytest -k end2end -m 'sanity_bnn or end2end or fpgadataflow or notebooks' --junitxml=end2end.xml --html=reports/end2end.html --reruns 1 --forked --dist loadfile -n $PYTEST_PARALLEL
+  pytest -k end2end -m 'sanity_bnn or end2end or fpgadataflow or notebooks' --junitxml=reports/end2end.xml --html=reports/end2end.html --reruns 2 --forked --dist loadfile -n $PYTEST_PARALLEL
 elif [ $1 = "full" ]; then
   echo "Running full test suite, each step with appropriate parallelism"
   $0 main;
