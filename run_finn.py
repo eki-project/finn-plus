cd5f584b67054ef77dee66eac587b02c3488b86e
bwintermann <bjarne.wintermann@me.com>
2025-02-28 10:50:09 +0100

    Add tests, help descriptions

--- run_finn.py
+++ run_finn.py
@@ -12,16 +12,25 @@ from interface.finn_deps import FINN_BOARDFILES, FINN_DEPS, deps_exist, pull_boa
 from interface.finn_envvars import GLOBAL_FINN_ENVVARS, generate_envvars
 
 
+########### TODO #############
+# - Copy .Xilinx to HOME dir when starting
+# - Vivado IP Cache env var (run-docker.sh)
+
+
 #### GROUPS ####
 
 @click.group()
 def main_group(): pass
 
-@click.group()
+@click.group(help="Dependency related commands. Use to check and update your non-python based FINN dependencies")
 def deps(): pass
+
+@click.group(help="Run tests on the given FINN installation. Use finn test --help to learn more.")
+def test(): pass
 
 
 #### DEPENDENCIES ####
+
 def update_deps(path):
     deppath = Path(path).absolute()
     console = Console()
@@ -62,58 +71,58 @@ def check_deps(path):
         sys.exit(1)
 
 
-@click.command("update")
-@click.option("--path", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie")
+@click.command("update", help="Install or update FINNs non-python dependencies")
+@click.option("--path", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie", show_default=True)
 def update_deps_command(path):
     update_deps(path)
 
 
-@click.command("install")
-@click.option("--path", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie")
-def install_deps_command(path):
-    update_deps(path)
-
-
-@click.command("check")
-@click.option("--path", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie")
+@click.command("check", help="Check for and print missing dependencies in the given path")
+@click.option("--path", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie", show_default=True)
 def check_deps_command(path):
     check_deps(path)
 
 
 
 #### BUILD ####
+
 def setup_envvars():
     console = Console()
-    console.print("Setting up environment variables...")
+    console.print()
+    first = True
+    newly_set = {}
     for varname, varcontent in GLOBAL_FINN_ENVVARS.items():
         if varname not in os.environ.keys():
-            ans = console.input(f"Variable [bold]{varname}[/bold] is not set. Default is [blue]{varcontent}[/blue] (1 - leave unset, 2 - set to default, 3 - set own) > ")
+            if first:
+                console.print(Panel("[yellow]Some environment variables required by FINN are not set. You can set these variables interactively now. To avoid this in the future, set them permanently, for example in your bashrc![/yellow]", title="[bold]Missing Environment Variables[/bold]"))
+                first = False
+            ans = console.input(f"\nVariable [bold]{varname}[/bold] is not set. Default is [blue]{varcontent}[/blue] (1 - leave unset, 2 - set to default, 3 - set own) > ")
             if ans == "1":
                 continue
             elif ans == "2":
                 os.environ[varname] = varcontent
+                newly_set[varname] = varcontent
             elif ans == "3":
                 os.environ[varname] = console.input("New Value > ")
+                newly_set[varname] = os.environ[varname]
             else:
                 console.print("[red]Unknown option. Stopping...[/red]")
                 sys.exit(1)
-
-@click.command()
-def setup():
-    setup_envvars()
+    
 
 
-
-@click.command()
+@click.command(help="Run a FINN build with the given build file")
 @click.argument("buildfile")
 @click.option("--force-update", "-f", help="Force an update of dependencies before starting", default=False, is_flag=True)
-@click.option("--deps-path", "-d", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie")
-@click.option("--local-temps", "-l", default=True, is_flag=True, help="Whether to store temporary build files local to the model/buildfile. Defaults to true")
-@click.option("--num-workers", "-n", default=-1, help="Number of workers to do parallel tasks")
+@click.option("--deps-path", "-d", default=str(Path.home() / ".finn" / "deps"), help="Path to directory where dependencies lie", show_default=True)
+@click.option("--local-temps", "-l", default=True, is_flag=True, help="Whether to store temporary build files local to the model/buildfile.")
+@click.option("--num-workers", "-n", default=-1, help="Number of workers to do parallel tasks. -1 automatically uses 75% of your available cores.", show_default=True)
 @click.option("--clean-temps", "-c", default=False, is_flag=True, help="Clean temporary files from previous runs automatically?")
-def build(buildfile, force_update, deps_path, local_temps, num_workers, clean_temps):
+@click.option("--ignore-missing-envvars", "-i", default=False, help="When using this flag, FINN does not interactively ask to set missing environment variables. Useful for starting FINN automatically without user input but may run into errors if variables are not set.", is_flag=True)
+def build(buildfile, force_update, deps_path, local_temps, num_workers, clean_temps, ignore_missing_envvars):
     # TODO: Keep usage of str vs Path() consistent everywhere
-
+    if not ignore_missing_envvars:
+        setup_envvars()
     console = Console()
     buildfile_path = Path(buildfile)
     if not buildfile_path.exists():
@@ -154,11 +163,26 @@ def build(buildfile, force_update, deps_path, local_temps, num_workers, clean_te
 
 
 
+#### TESTS ####
+@click.command(name="quicktest", help="Run the quicktests in FINN. Should only take a few minutes")
+@click.option("--variant", "-v", help="Which variant of the quicktests to execute. Defaults to standard tests.", default="")
+def run_quicktest():
+    setup_envvars()
+
+    subprocess.run("pytest -m 'not (vivado or slow or vitis or board or notebooks or bnn_pynq)' --dist=loadfile -n auto", shell=True) 
+
+
+
+
+
+############### CLICK ###############
+
+test.add_command(run_quicktest)
 deps.add_command(update_deps_command)
 deps.add_command(check_deps_command)
 main_group.add_command(deps)
-main_group.add_command(setup)
 main_group.add_command(build)
+main_group.add_command(test)
 
 if __name__ == "__main__":
     main_group()
