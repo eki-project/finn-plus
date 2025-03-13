bd72f1c4bc6b05bf44615cbf6b43e9b5ea6a3f0b
Felix Jentzsch <felix.jentzsch@upb.de>
2025-03-13 18:03:55 +0100

    [CI] Switch from Singularity to Poetry-based setup

--- interface/manage_tests.py
+++ interface/manage_tests.py
@@ -1,6 +1,7 @@
 import os
 import shlex
 import subprocess
+import sys
 from pathlib import Path
 
 from interface.interface_globals import IS_POSIX
@@ -32,12 +33,42 @@ def run_test(variant: str, num_workers: str) -> None:
         case "end2end":
             subprocess.run("pytest -k end2end", shell=True)
         case "full":
-            subprocess.run(
+            test_1_process = subprocess.Popen(
                 shlex.split(
-                    f"pytest -k 'not (rtlsim or end2end)' --dist=loadfile -n {num_workers}",
+                    (
+                        "pytest -m 'not (end2end or sanity_bnn or notebooks)' "
+                        "--junitxml=$CI_PROJECT_DIR/reports/main.xml "
+                        "--html=$CI_PROJECT_DIR/reports/main.html "
+                        f"--reruns 1 --dist worksteal -n {num_workers}"
+                    ),
                     posix=IS_POSIX,
                 )
             )
-            subprocess.run(shlex.split(f"pytest -k rtlsim --workers {num_workers}", posix=IS_POSIX))
-            subprocess.run("pytest -k end2end", shell=True)
+            test_2_process = subprocess.Popen(
+                shlex.split(
+                    (
+                        "pytest -m 'end2end or sanity_bnn or notebooks' "
+                        "--junitxml=$CI_PROJECT_DIR/reports/end2end.xml "
+                        "--html=$CI_PROJECT_DIR/reports/end2end.html "
+                        f"--reruns 1 --dist loadgroup -n {num_workers}"
+                    ),
+                    posix=IS_POSIX,
+                )
+            )
+            test_1_process.communicate()
+            test_1_returncode = test_1_process.returncode
+            test_2_process.communicate()
+            test_2_returncode = test_2_process.returncode
+            if test_1_returncode or test_2_returncode:
+                sys.exit(1)
+
+            subprocess.run(
+                shlex.split(
+                    (
+                        "pytest_html_merger -i $CI_PROJECT_DIR/reports/ "
+                        "-o $CI_PROJECT_DIR/reports/full_test_suite.html"
+                    ),
+                    posix=IS_POSIX,
+                )
+            )
     os.chdir(original_dir)
