cb0b3adf08e7ffaab4975b1000efccfb47ca68a6
bwintermann <bjarne.wintermann@me.com>
2025-03-21 17:08:02 +0100

    Temporary fix for subprocess not using the correct python executable

--- interface/manage_tests.py
+++ interface/manage_tests.py
@@ -12,11 +12,12 @@ def run_test(variant: str, num_workers: str) -> None:
     """Run a given test variant with the given number of workers"""
     original_dir = Path.cwd()
     os.chdir(Path(__file__).parent.parent)
+    python_prefix = str(Path(os.environ["VIRTUAL_ENV"]) / "bin" / "python3")
     match variant:
         case "quick":
             subprocess.run(
                 shlex.split(
-                    f"pytest -m 'not "
+                    f"{python_prefix} -m pytest -m 'not "
                     f"(vivado or slow or vitis or board or notebooks or bnn_pynq)' "
                     f"--dist=loadfile -n {num_workers}",
                     posix=IS_POSIX,
@@ -25,19 +26,24 @@ def run_test(variant: str, num_workers: str) -> None:
         case "main":
             subprocess.run(
                 shlex.split(
-                    f"pytest -k 'not (rtlsim or end2end)' --dist=loadfile -n {num_workers}",
+                    f"{python_prefix} -m pytest -k 'not (rtlsim or end2end)' "
+                    f"--dist=loadfile -n {num_workers}",
                     posix=IS_POSIX,
                 )
             )
         case "rtlsim":
-            subprocess.run(shlex.split(f"pytest -k rtlsim --workers {num_workers}", posix=IS_POSIX))
+            subprocess.run(
+                shlex.split(
+                    f"{python_prefix} -m pytest -k rtlsim --workers {num_workers}", posix=IS_POSIX
+                )
+            )
         case "end2end":
             subprocess.run("pytest -k end2end", shell=True)
         case "full":
             test_1_process = subprocess.Popen(
                 shlex.split(
                     (
-                        "pytest -m 'not (end2end or sanity_bnn or notebooks)' "
+                        f"{python_prefix} -m pytest -m 'not (end2end or sanity_bnn or notebooks)' "
                         "--junitxml=$CI_PROJECT_DIR/reports/main.xml "
                         "--html=$CI_PROJECT_DIR/reports/main.html "
                         f"--reruns 1 --dist worksteal -n {num_workers}"
@@ -48,7 +54,7 @@ def run_test(variant: str, num_workers: str) -> None:
             test_2_process = subprocess.Popen(
                 shlex.split(
                     (
-                        "pytest -m 'end2end or sanity_bnn or notebooks' "
+                        f"{python_prefix} -m pytest -m 'end2end or sanity_bnn or notebooks' "
                         "--junitxml=$CI_PROJECT_DIR/reports/end2end.xml "
                         "--html=$CI_PROJECT_DIR/reports/end2end.html "
                         f"--reruns 1 --dist loadgroup -n {num_workers}"
@@ -64,7 +70,7 @@ def run_test(variant: str, num_workers: str) -> None:
             subprocess.run(
                 shlex.split(
                     (
-                        "pytest_html_merger -i $CI_PROJECT_DIR/reports/ "
+                        f"{python_prefix} -m pytest_html_merger -i $CI_PROJECT_DIR/reports/ "
                         "-o $CI_PROJECT_DIR/reports/full_test_suite.html"
                     ),
                     posix=IS_POSIX,
