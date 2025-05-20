a49d003cb67023e8370522e6315906dc63cd0201
Felix Jentzsch <felix.jentzsch@upb.de>
2025-05-20 14:29:57 +0200

    Introduce custom step library

--- src/finn/benchmarking/dut/vgg10.py
+++ src/finn/benchmarking/dut/vgg10.py
@@ -1,23 +1,5 @@
-from qonnx.core.modelwrapper import ModelWrapper
 from finn.builder.build_dataflow_config import DataflowBuildConfig
-from qonnx.transformation.change_3d_tensors_to_4d import Change3DTo4DTensors
-from qonnx.transformation.general import GiveUniqueNodeNames
-import finn.transformation.fpgadataflow.convert_to_hw_layers as to_hw
-import finn.transformation.streamline.absorb as absorb
-
 from finn.benchmarking.bench_base import bench
-
-
-def step_pre_streamline(model: ModelWrapper, cfg: DataflowBuildConfig):
-    model = model.transform(Change3DTo4DTensors())
-    model = model.transform(absorb.AbsorbScalarMulAddIntoTopK())
-    return model
-
-def step_convert_final_layers(model: ModelWrapper, cfg: DataflowBuildConfig):
-    model = model.transform(to_hw.InferChannelwiseLinearLayer())
-    model = model.transform(to_hw.InferLabelSelectLayer())
-    model = model.transform(GiveUniqueNodeNames())
-    return model
 
 class bench_vgg10(bench):
     def step_build_setup(self):
