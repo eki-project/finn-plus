720939829a8f38bdf326ca59e2ef2fb8fb409ac7
bwintermann <bjarne.wintermann@me.com>
2025-08-01 16:24:56 +0200

    Fixed dep commit, changed test name, wip adding blackboxfunction

---
+++ tests/fpgadataflow/test_fpgadataflow_demux.py
@@ -0,0 +1,13 @@
+import pytest
+from finn.custom_op.fpgadataflow.hls.demux import *
+
+@pytest.mark.fpgadataflow
+@pytest.mark.vivado
+def test_fpgadataflow_arbiter_mux() -> None:
+    raise AssertionError()
+
+
+@pytest.mark.fpgadataflow
+@pytest.mark.vivado
+def test_fpgadataflow_arbiter_demux() -> None:
+    raise AssertionError()
