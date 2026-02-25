c0448b781616486a11def9a29e2f2b34bf7049c9
Linus Jungemann <38974033+LinusJungemann@users.noreply.github.com>
2026-02-25 15:31:35 +0100

    Adding deprecated and deprecating AddStreams, Channelwise_OP, DuplicateStream and StreamingEltwise (#166)

--- src/finn/custom_op/fpgadataflow/channelwise_op.py
+++ src/finn/custom_op/fpgadataflow/channelwise_op.py
@@ -33,6 +33,7 @@ from qonnx.core.datatype import DataType
 from qonnx.util.basic import qonnx_make_model
 
 from finn.custom_op.fpgadataflow.hwcustomop import HWCustomOp
+from finn.util.deprecated import deprecated
 from finn.util.logging import log
 
 # ONNX i/o tensor shape assumptions for channelwise ops:
@@ -76,6 +77,7 @@ def get_smallest_possible(vals):
 class ChannelwiseOp(HWCustomOp):
     """Abstraction layer for HW implementation of ChannelwiseOp."""
 
+    @deprecated
     def __init__(self, onnx_node, **kwargs):
         super().__init__(onnx_node, **kwargs)
 
